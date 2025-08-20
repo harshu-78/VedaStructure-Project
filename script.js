f4d2944064799df92443f752011b7a8dbfec2df6
harshu-78 <harshudev3@gmail.com>
2025-08-20 19:53:35 +0530

    code update

--- script.js
+++ script.js
@@ -20,9 +20,50 @@ document.addEventListener("click", (e) => {
 });
 
 
-var banner = new banner(".mybanner", {
+const menuBtn = document.getElementById("menu-btn");
+  const mobileMenu = document.getElementById("mobile-menu");
+
+  menuBtn.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden");
+  });
+
+  // Dropdown toggle inside mobile menu
+  document.querySelectorAll(".dropdown-toggle").forEach(btn => {
+    btn.addEventListener("click", () => {
+      const submenu = btn.nextElementSibling;
+      submenu.classList.toggle("hidden");
+    });
+  });
+
+
+
+var swiper = new Swiper(".myswiper", {
   loop: true,
-  autoplay:{ delay:25000, disableOnInteraction:false },
-  pagination:{ el: ".banner-pagination", clickable:true },
-  navigation:{ nextEl: ".banner-button-next", prevEl: ".banner-button-prev" }
+  autoplay: { delay: 2500, disableOnInteraction: false },
+  pagination: { el: ".swiper-pagination", clickable: true },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  }
 });
+
+
+
+  $(document).ready(function(){
+    $('.slick-slider').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      arrows: true,
+      prevArrow: $('.slick-prev'),
+      nextArrow: $('.slick-next'),
+      responsive: [
+        { breakpoint: 768, settings: { slidesToShow: 2 } },
+        { breakpoint: 480, settings: { slidesToShow: 1 } }
+      ]
+    });
+  });
+
+
+
